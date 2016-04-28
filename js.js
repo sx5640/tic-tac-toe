5a3751ba049a6b3acb4a51fbae64aa6c70b10ef3
sx5640 <sx5640@msn.com>
2016-04-27 23:34:40 -0400

    now you can play this with a smarter bot, who will try to stop you and win

--- js.js
+++ js.js
@@ -1,5 +1,7 @@
 $(document).on('ready',function () {
   var turn = 0;
+  var winner = '';
+  var winnerCells = [];
 
   var judge = function (player) {
     var my_cells = $('.' + player);
@@ -7,12 +9,8 @@ $(document).on('ready',function () {
       if (my_cells[i] !== self){
         for (var j = 0; j < my_cells.size(); j++) {
           if (j !==i && my_cells[j]!== self && parseInt($(self).attr('hiddenattr'))+parseInt($(my_cells[i]).attr('hiddenattr'))+parseInt($(my_cells[j]).attr('hiddenattr')) === 15) {
-            $('.score').text(player+' WINS');
-            $(self).css('color', 'tomato');
-            $(my_cells[i]).css('color', 'tomato');
-            $(my_cells[j]).css('color', 'tomato');
-            $('td').off('click');
-            $('.game_over').fadeIn();
+            winner = player;
+            winnerCells = Array(self, my_cells[i], my_cells[j])
             break;
             break;
           }
@@ -25,11 +23,21 @@ $(document).on('ready',function () {
     }
   }
 
+  var announcement = function (player) {
+    if (winner) {
+      $('.score').text(player+' WINS');
+      $(winnerCells).css('color', 'tomato');
+      // $(my_cells[i]).css('color', 'tomato');
+      // $(my_cells[j]).css('color', 'tomato');
+      $('td').off('click');
+      $('.game_over').fadeIn();
+    }
+  }
 
   var bot = function () {
-    opponentCells = $('.X').toArray();
-    botCells = $('.O').toArray();
-    emptyCells = $('td');
+    var opponentCells = $('.X').toArray();
+    var botCells = $('.O').toArray();
+    var emptyCells = $('td');
     for (var i in opponentCells) {
       emptyCells = emptyCells.not(opponentCells[i]);
     }
@@ -44,6 +52,51 @@ $(document).on('ready',function () {
     $(self).off('click');
   }
 
+  var smarterBot = function () {
+    var opponentCells = $('.X').toArray();
+    var botCells = $('.O').toArray();
+    var emptyCells = $('td');
+    for (var i in opponentCells) {
+      emptyCells = emptyCells.not(opponentCells[i]);
+    }
+    for (var j in botCells) {
+      emptyCells = emptyCells.not(botCells[j]);
+    }
+    var found = false;
+    emptyCells = emptyCells.toArray();
+    for (var k in emptyCells) {
+      self = emptyCells[k];
+      if (turn > 4) {
+        judge('O');
+      }
+      if (winner) {
+        found = true;
+        winner = '';
+        break;
+      }
+    }
+    if (found !== true ) {
+      for (var k in emptyCells) {
+        self = emptyCells[k];
+        if (turn >= 3) {
+          judge('X');
+        }
+        if (winner) {
+          found = true;
+          winner = ''
+          break;
+        }
+      }
+    }
+    if (found !== true ) {
+      var outpuNum = Math.round(Math.random() * (emptyCells.length - 1));
+      self = $(emptyCells[outpuNum])[0];
+    }
+    $(self).text('O');
+    $(self).addClass('O');
+    $(self).off('click');
+  }
+
   var vsBot = function () {
     $('.game_on').fadeOut();
     $('td').on('click', function () {
@@ -53,13 +106,15 @@ $(document).on('ready',function () {
       $(this).off('click');
       if (turn >= 4) {
         judge('X');
+        announcement('X');
       }
       turn++;
 
-      if ($('.score').text() !== 'X WINS') {
-        bot();
+      if (winner === '') {
+        smarterBot();
         if (turn > 4) {
           judge('O');
+          announcement('O');
         }
         turn++;
       }
@@ -83,6 +138,7 @@ $(document).on('ready',function () {
       }
       if (turn >= 4) {
         judge($(this).attr('class'));
+        announcement($(this).attr('class'));
       }
       turn++;
     })
@@ -94,6 +150,8 @@ $(document).on('ready',function () {
     $('td').removeClass();
     $('td').css('color','black');
     $('.score').empty();
+    winner = '';
+    winnerCells = [];
     $('.game_over').fadeOut();
     $('.game_on').fadeIn();
   }
